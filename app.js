1464211acb68f3d05536d0d4a978931edc4abb3b
Maruthi Pothuganti <maruthicharypothuganti@gmail.com>
2021-08-16 12:42:46 +0530

    initialcommit

---
+++ app.js
@@ -0,0 +1,17 @@
+const input = document.querySelectorAll(".input");
+const btnCheck = document.querySelector("#btnCheck");
+const output = document.querySelector("#output");
+
+function showOutput(msg) {
+    output.innerHTML = msg;
+}
+
+
+btnCheck.addEventListener('click', function isTriangle() {
+    const userInput = Number(input[0].value) + Number(input[1].value) + Number(input[2].value);
+    if (userInput === 180) {
+        showOutput("It is a Triangle");
+    } else {
+        showOutput("Uh oh, It is not a Triangle");
+    }
+})
