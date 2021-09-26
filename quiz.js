8af7689c8b4feb8286516407fdb1cfe11cce0c41
Maruthi Pothuganti <maruthicharypothuganti@gmail.com>
2021-09-26 18:41:43 +0530

    fix

--- quiz.js
+++ quiz.js
@@ -8,16 +8,21 @@ btnFinish.addEventListener("click", function Result() {
     const fromQuiz = new FormData(quiz);
     let score = 0;
     let index = 0;
+    let count = 0;
     for (let userAnswers of fromQuiz.values()) {
         if (userAnswers === correctAnswers[index]) {
             score += 1;
         }
         index += 1;
+        count = count + 1;
     }
 
-    if (score === 0) {
-        alert("Please provide all inputs");
-    } else {
+    if (count === 10) {
         result.innerText = "Your Total Score is : " + score;
+    } else {
+
+        alert("Please provide all inputs");
     }
+
+
 })
